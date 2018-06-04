2e6a34d5085fcc6edd6261f4efa6c9af608c91bc
Sammo <sammo.wong@foxmail.com>
2018-06-05 01:00:59 +0800

    init login page

---
+++ Focus.Api/Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Focus.Api.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Account")]
+    public class AccountController : Controller
+    {
+        public async Task<IActionResult> VerificationCode()
+        {
+            return Ok();
+        }
+    }
+}
