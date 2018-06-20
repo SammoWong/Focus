b6f4bf3b5d195349c8a1012d60c1dfead2c56562
Sammo <sammo.wong@foxmail.com>
2018-06-21 01:16:59 +0800

    add unitofwork

---
+++ Focus.Domain/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Focus.Domain
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        void Commit();
+
+        void Rollback();
+
+        void SaveChanges();
+    }
+}
