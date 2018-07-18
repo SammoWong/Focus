09d62b513d7e172f8831abf5baa1a134031a3ee2
Sammo <sammo.wong@foxmail.com>
2018-07-18 10:50:02 +0800

    添加前端样式和js插件

---
+++ Focus.Web/wwwroot/js/loading.init.js
@@ -0,0 +1,21 @@
+﻿function showLoading() {
+    $('body').loading({
+        loadingWidth: 120,
+        title: '',
+        name: 'loading',
+        discription: '',
+        direction: 'column',
+        type: 'origin',
+        // originBg:'#71EA71',
+        originDivWidth: 40,
+        originDivHeight: 40,
+        originWidth: 6,
+        originHeight: 6,
+        smallLoading: false,
+        loadingMaskBg: 'rgba(0,0,0,0.2)'
+    });
+}
+
+function hideLoading() {
+    removeLoading('loading');
+}
---
+++ Focus.Web/wwwroot/js/toastr.init.js
@@ -0,0 +1,1 @@
+﻿toastr.options = { closeButton: true };
