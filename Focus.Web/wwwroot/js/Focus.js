0a8cc5e8a54d871e963ebb8cd63cdc763b62e823
Sammo <sammo.wong@foxmail.com>
2018-07-22 04:18:43 +0800

    重写focus.js

--- Focus.Web/wwwroot/js/Focus.js
+++ Focus.Web/wwwroot/js/Focus.js
@@ -1,79 +1,134 @@
-﻿(function () {
+﻿; (function () {
     "use strict";
-    var root = this,
-        store,
-        doc;
-    var Focus = {};
+    var GLOBAL = typeof global == "object" ? global : window;
 
-    if (typeof module !== 'undefined' && module.exports) {
-        module.exports = Focus;
-    } else {
-        root.Focus = Focus;
-        doc = root.document;
-        if ((typeof Storage) !== 'undefined') {
-            store = root.localStorage;
+    //原型方法
+    var _toString = Object.prototype.toString;
+
+
+    /**
+     * 判断是否为函数
+     * @param {any} value
+     */
+    function isFunction(value) {
+        return _toString.call(value) === "[object Function]";
+    }
+
+    /**
+     * 判断是否为对象
+     * @param {any} value
+     */
+    function isObject(value) {
+        return obj && _toString.call(value) === "[object Object]";
+    }
+
+    /**
+     * 判断是否为数组
+     * @param {any} value
+     */
+    function isArray(value) {
+        return _toString.call(value) === "[object Array]";
+    }
+
+    /**
+     * 判断是否为字符串
+     * @param {any} value
+     */
+    function isString(value) {
+        return _toString.call(value) === "[object String]"
+    }
+
+    /**
+     * 判断是否为数字
+     * @param {any} value
+     */
+    var isNumber = function (value) {
+        return _toString.call(value) === '[object Number]';
+    };
+
+    /**
+     * 判断是否为布尔值
+     * @param {any} value
+     */
+    var isBoolean = function (value) {
+        return _toString.call(value) === '[object Boolean]';
+    };
+
+    /**
+     * 判断是否为空
+     * @param {any} value
+     */
+    var isNull = function (value) {
+        return _toString.call(value) === '[object Null]';
+    };
+
+    /**
+     * 判断是否为undefined
+     * @param {any} value
+     */
+    var isUndefined = function (value) {
+        return _toString.call(value) === '[object Undefined]';
+    };
+
+    /**
+     * 判断是否为Date类型
+     * @param {any} value
+     */
+    var isDate = function (value) {
+        return _toString.call(value) === '[object Date]'
+    };
+
+    /**
+     * 去除字符串左右空格
+     * @param {any} value
+     */
+    var trim = function (value) {
+        if (!isString(value)) {
+            return value;
         }
+        return value.replace(/(^\s*)|(\s*$)/g, '');
+    };
+
+    /**
+     * 去除字符串中的空格
+     * @param {any} value
+     */
+    var clearSpace = function (value) {
+        if (!isString(value)) {
+            return value;
+        }
+        return value.replace(/[ ]/g, '');
     }
 
-    //判断是否为数组
-    Focus.isArray = function (obj) {
-        return Object.prototype.toString.call(obj) === '[object Array]';
-    };
-
-    //判断是否为函数
-    Focus.isFunction = function (func) {
-        return Object.prototype.toString.call(func) === '[object Array]';
+    /**
+     * 判断字符串是否包含中文
+     * @param {any} value
+     */
+    var existCN = function (value) {
+        if (!isString(value)) {
+            return value;
+        }
+        return /.*[\u4e00-\u9fa5]+.*$/.test(value);
     }
 
-    //判断是否为对象
-    Focus.isObject = function (obj) {
-        return Object.prototype.toString.call(obj) === '[object Object]';
+    var _ = {
+        isFunction: isFunction,
+        isObject: isObject,
+        isArray: isArray,
+        isString: isString,
+        isNumber: isNumber,
+        isBoolean: isBoolean,
+        isNull: isNull,
+        isUndefined: isUndefined,
+        isDate: isDate,
+        trim: trim,
+        clearSpace: clearSpace,
+        existCN: existCN,
     };
 
-    //判断是否为字符串
-    Focus.isString = function (obj) {
-        return Object.prototype.toString.call(obj) === '[object String]';
-    };
+    GLOBAL._ = _;
 
-    //判断是否为数值
-    Focus.isNumber = function (obj) {
-        return Object.prototype.toString.call(obj) === '[object Number]';
-    };
-
-
-    //判断是否为布尔值
-    Focus.isBoolean = function (obj) {
-        return Object.prototype.toString.call(obj) === '[object Boolean]';
-    };
-
-
-    //判断是否为Date类型
-    Focus.isDate = function (obj) {
-        return Object.prototype.toString.call(obj) === '[object Date]';
-    };
-
-
-    //判断是否为null、undefined或者空
-    Focus.isNull = function (value) {
-        return value === '' || value === undefined || value === null ? true : false;
-    };
-
-    //判断是否为URL
-    Focus.isURL = function (text) {
-        var reg = /[a-zA-z]+:\/\/[^\s]/;
-        return reg.test(text);
-    };
-
-    //过滤字符串中的空格
-    Focus.clearSpace = function (text) {
-        return text.replace(/[ ]/g, "");
-    };
-
-    //判断字符串中是否包含中文
-    Focus.existCN = function (text) {
-        var reg = /.*[\u4e00-\u9fa5]+.*$/;
-        return reg.test(text);
-    };
-
-
-}.call(this));
+    if (typeof module === "object" && typeof module.exports === "object") {
+        module.exports = _;
+    }
+})();
