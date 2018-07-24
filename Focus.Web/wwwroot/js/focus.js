c63887f99d70cb7e8ed47bcab6fd0426cfebba89
Sammo <sammo.wong@foxmail.com>
2018-07-24 16:23:55 +0800

    update focus.js

--- Focus.Web/wwwroot/js/focus.js
+++ Focus.Web/wwwroot/js/focus.js
@@ -80,7 +80,7 @@
 
     /**
      * 去除字符串左右空格
-     * @param {any} value
+     * @param {string} value
      */
     var trim = function (value) {
         if (!isString(value)) {
@@ -91,26 +91,103 @@
 
     /**
      * 去除字符串中的空格
-     * @param {any} value
+     * @param {string} value
      */
     var clearSpace = function (value) {
         if (!isString(value)) {
             return value;
         }
         return value.replace(/[ ]/g, '');
-    }
+    };
 
     /**
      * 判断字符串是否包含中文
-     * @param {any} value
+     * @param {string} value
      */
     var existCN = function (value) {
         if (!isString(value)) {
             return value;
         }
         return /.*[\u4e00-\u9fa5]+.*$/.test(value);
-    }
+    };
 
+    /**
+     * 获取当前日期（年月日）
+     * type:CN中文格式
+     * @param {any} type
+     */
+    var getDate = function (type) {
+        var time = new Date(),
+            year = time.getFullYear(),
+            m = time.getMonth() + 1,
+            month = m < 10 ? ('0' + m) : m,
+            d = time.getDate(),
+            day = d < 10 ? ('0' + d) : d,
+            dateText = '';
+
+        if (type === 'CN') {
+            return dateText += year + '年' + month + '月' + day + '日';
+        } else {
+            return dateText += year + '-' + month + '-' + day;
+        }
+    };
+
+    /**
+     * 获取当前时间（时分秒）
+     * type:CN中文格式
+     * @param {any} type
+     */
+    var getTime = function (type) {
+        var time = new Date(),
+            hours = time.getHours(),
+            m = time.getMinutes(),
+            minutes = m < 10 ? ('0' + m) : m,
+            s = time.getSeconds(),
+            seconds = s < 10 ? ('0' + s) : s,
+            dateText = '';
+
+        if (type === 'CN') {
+            return dateText += hours + '时' + minutes + '分' + seconds + '秒';
+        } else {
+            return dateText += hours + ':' + minutes + ':' + seconds;
+        }
+    };
+
+
+    /**
+     * 根据日期获取当前星期，为空则获取当前时间星期
+     * @param {any} date
+     */
+    var getWeek = function (date) {
+        date = date || (getDate());
+        var digitText = toCnDigit(new Date(date).getDay());
+        digitText == '零' ? digitText = '天' : digitText = digitText;
+        return '星期' + digitText;
+    };
+
+    /**
+     * 阿拉伯数字转中文数字
+     * @param {any} text
+     */
+    var toCnDigit = function (value) {
+        var charArr = ['零', '一', '二', '三', '四', '五', '六', '七', '八', '九', '十'],
+            num = isNumber(value) ? value.toString() : '',
+            numArr = num.split(''),
+            len = numArr.length,
+            result = '';
+
+        for (var i = 0; i < len; i++) {
+            result += charArr[parseInt(numArr[i])];
+        }
+        return result;
+    };
+
+    /**
+     * GET请求
+     * @param {any} url
+     * @param {any} data
+     * @param {any} success
+     */
     var get = function (url, data, success) {
         var conf;
         if (isFunction(data)) {
@@ -120,9 +197,15 @@
         }
         conf.headers = { 'Authorization': "Bearer " + access_token };
         $.ajax(conf);
-    }
+    };
 
-    var post = function (url, data, beforeSend, success, error) {
+    /**
+     * POST请求
+     * @param {any} url
+     * @param {any} data
+     * @param {any} success
+     */
+    var post = function (url, data, success) {
         var conf;
         if (isFunction(data)) {
             conf = { url: url, method: 'post', success: data, async: true, dataType: 'json' };
@@ -131,7 +214,121 @@
         }
         conf.headers = { 'Authorization': "Bearer " + access_token };
         $.ajax(conf);
+    };
+
+    /**
+     * 设置cookie
+     * @param {string} key
+     * @param {string} value
+     * @param {number} time
+     */
+    var _setCookie = function (key, value, time) {
+        var now = new Date();
+        var expires = new Date(now.getTime() + time * 1000);
+        document.cookie = key + '=' + value + ';' + 'expires=' + expires.toUTCString();
+    };
+
+    /**
+     * 读取cookie
+     * @param {string} key
+     */
+    var _getCookie = function (key) {
+        var name = key + "=";
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var c = cookies[i].trim();
+            if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+        }
+        return "";
+    };
+
+    /**
+     * 根据key移除cookie
+     * @param {string} key
+     */
+    var _removeCookie = function (key) {
+        _setCookie(key, '', -1);
     }
+
+    /**
+     * 清除所有cookie
+     * */
+    var _clearCookie = function () {
+        var cookies = document.cookie ? document.cookie.split(';') : [];
+        for (var i = 0; i < cookies.length; i++) {
+            var key = cookies[i].split('=');
+            _removeCookie(key[0]);
+        }
+    }
+
+    var Cookie = {
+        set: _setCookie,
+        get: _getCookie,
+        remove: _removeCookie,
+        clear: _clearCookie,
+    };
+
+    /**
+     * 设置localStorage本地缓存
+     * @param {any} key
+     * @param {any} value
+     */
+    var _setLocalStorage = function (key, value) {
+        localStorage.setItem(key, value);
+    };
+
+    /**
+     * 获取localStorage本地缓存
+     * @param {any} key
+     */
+    var _getLocalStorage = function (key) {
+        return localStorage.getItem(key);
+    };
+
+    /**
+     * 删除localStorage本地缓存
+     * @param {any} key
+     */
+    var _removeLocalStorage = function (key) {
+        localStorage.removeItem(key);
+        return _getLocalStorage(key) === null ? true : false;
+    };
+
+    /**
+     * 清空localStorage本地缓存
+     * */
+    var _clearLocalStorage = function () {
+        localStorage.clear();
+        return true;
+    }
+
+    var Storage = {
+        setLocalStorage: _setLocalStorage,
+        getLocalStorage: _getLocalStorage,
+        removeLocalStorage: _removeLocalStorage,
+        clearLocalStorage: _clearLocalStorage
+    };
+
+    var View = {
+        /**
+         * 获取可用宽高
+         * */
+        getSize: function () {
+            return { width: document.body.clientWidth, height: document.body.clientHeight };
+        },
+        /**
+         * 获取可用宽度
+         * */
+        getWidth: function () {
+            return document.body.clientWidth;
+        },
+        /**
+         * 获取可用高度
+         * */
+        getHeight: function () {
+            return document.body.clientHeight;
+        }
+    };
 
     var _ = {
         isFunction: isFunction,
@@ -146,8 +343,15 @@
         trim: trim,
         clearSpace: clearSpace,
         existCN: existCN,
+        getDate: getDate,
+        getTime: getTime,
+        getWeek: getWeek,
+        toCnDigit: toCnDigit,
         get: get,
         post: post,
+        Cookie: Cookie,
+        Storage: Storage,
+        View: View,
     };
 
     GLOBAL._ = _;
