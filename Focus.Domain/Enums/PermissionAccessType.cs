81a2cbbbe98414cd8c3fa7539353c8d52d826806
Sammo <sammo.wong@foxmail.com>
2018-08-20 16:27:11 +0800

    添加jquery-steps插件

---
+++ Focus.Domain/Enums/PermissionAccessType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Focus.Domain.Enums
+{
+    public enum PermissionAccessType : byte
+    {
+        [Display(Name = "模块")]
+        Module = 1,
+
+        [Display(Name = "按钮")]
+        Button = 2
+    }
+}
---
+++ Focus.Domain/Enums/PermissionMasterType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Focus.Domain.Enums
+{
+    public enum PermissionMasterType : byte
+    {
+        [Display(Name = "角色")]
+        Role = 1,
+
+        [Display(Name = "用户")]
+        User = 2
+    }
+}
